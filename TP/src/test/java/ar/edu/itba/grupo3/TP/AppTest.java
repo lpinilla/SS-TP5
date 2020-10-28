bd816073740a5e9d843a1963fb761decbca6080c
Lautaro Pinilla <lpinilla@itba.edu.ar>
2020-10-28 11:09:10 -0300

    initial Commit

---
+++ TP/src/test/java/ar/edu/itba/grupo3/TP/AppTest.java
@@ -0,0 +1,20 @@
+package ar.edu.itba.grupo3.TP;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
