939dd863b7d803652c830d7c7d07aed6a00e9b15
Micaela Banfi <mbanfi@itba.edu.ar>
2020-11-07 17:15:14 -0300

    nuevo calculo para el promedio de las velocidades

--- TP/plots/VeloVsDen.py
+++ TP/plots/VeloVsDen.py
@@ -3,18 +3,20 @@ import pandas as pd
 import numpy as np
 
 filename1="../resources/veloVsDensiL10H3.tsv"
-my_csv = pd.read_csv(filename1,sep='    ',header=None,usecols=[0,1], names=['densi1','velo1'])
+my_csv = pd.read_csv(filename1,sep=' ',header=None,usecols=[0,1,2], names=['densi1','velo1','error1'])
 velo1 = my_csv.velo1
 densi1 = my_csv.densi1
+error1=my_csv.error1
 
-filename2="../resources/veloVsDensiL8H5.tsv"
-my_csv = pd.read_csv(filename2,sep='    ',header=None,usecols=[0,1], names=['densi2','velo2'])
-velo2 = my_csv.velo2
-densi2 = my_csv.densi2
+# filename2="../resources/veloVsDensiL8H5.tsv"
+# my_csv = pd.read_csv(filename2,sep=' ',header=None,usecols=[0,1,2], names=['densi2','velo2','error2'])
+# velo2 = my_csv.velo2
+# densi2 = my_csv.densi2
+# error2= my_csv.error2
 
-plt.ylabel(r'Promedio de Velocidad de las particulas $[\frac{m}{s}]$')
+plt.ylabel(r'Velocidad  $[\frac{m}{s}]$')
 plt.xlabel(r'Densidad $[\frac{1}{m^{2}}]$')
-plt.plot(densi1,velo1,'ro-')
-plt.plot(densi2,velo2,'bo-')
-plt.legend(['Largo:10 Ancho:3','Largo:8 Ancho:5'])
+plt.errorbar(densi1, velo1, error1, linestyle='-', marker='.', color='red')
+# plt.errorbar(densi2, velo2, error2, linestyle='None', marker='.', color='blue')
+plt.legend(['Largo:10 Ancho:3'])
 plt.show()
